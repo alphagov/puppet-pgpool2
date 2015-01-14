251a455271b038217f2522085501cf3efd5ad9ed
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2015-01-14 15:16:14 +0000

    Fix spec tests
    - remove redhat tests since we dont support it.
    - fix package and service names to match what module currently does.

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -1,19 +1,12 @@
 require 'spec_helper'
 
 describe 'pgpool2::install' do
-  describe 'pgpool2::install class on RedHat' do
-    let(:facts) {{
-      :osfamily => 'RedHat',
-    }}
-
-    it { should contain_package('foo') }
-  end
 
   describe 'pgpool2::install class on Debian' do
     let(:facts) {{
       :osfamily => 'Debian',
     }}
 
-    it { should contain_package('foo') }
+    it { should contain_package('pgpool2') }
   end
 end
--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -1,20 +1,12 @@
 require 'spec_helper'
 
 describe 'pgpool2::service' do
-  describe 'pgpool2::service class on RedHat' do
-    let(:facts) {{
-      :osfamily => 'RedHat',
-    }}
-
-    it { should contain_service('foo') }
-  end
-
   describe 'pgpool2::service class on Debian' do
     let(:facts) {{
       :osfamily => 'Debian',
     }}
 
-    it { should contain_service('foo') }
+    it { should contain_service('pgpool2') }
   end
 end
 
